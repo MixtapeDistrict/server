644790c4a804078e0e107cd1695ae1dd760293b3
Chase <wakendarky@gmail.com>
2015-04-10 17:27:28 +1000

    Incorporated html with rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 WebInfo2::Application.routes.draw do
+
+  # get 'home/index'
+  match ':controller(/:action(/:id))', :via => :get
+
   resources :medium_comments
 
   resources :media
@@ -15,7 +19,7 @@ WebInfo2::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
