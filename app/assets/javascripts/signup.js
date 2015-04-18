b520c613d2a4ca767c315ba4abe5513c3a2a735f
RohanSharma1994 <RohanSharma1994@hotmail.com>
2015-04-18 21:27:16 +1000

    Added signup.js which will handle AJAX requests while sign up

---
+++ app/assets/javascripts/signup.js
@@ -0,0 +1,13 @@
+/* The following javascript file controls behaviour
+ * when signing up. Signing up is done using AJAX.
+ */
+function signUp() {
+	
+}
+
+/* A function which clears all error divs. */
+function clearMessages() {
+	document.getElementById('email-message').innerHTML = "";
+	document.getElementById('password-message').innerHTML = "";
+	document.getElementById('email-message').innerHTML = "";
+}
