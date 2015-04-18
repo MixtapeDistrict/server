981bf6aa31fa4a1cf9cbd69adf033b042fb8cc8d
RohanSharma1994 <RohanSharma1994@hotmail.com>
2015-04-18 21:50:57 +1000

    Changed get to post for sign_up ajax route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ WebInfo2::Application.routes.draw do
   # get 'home/index'
   root 'home#showHome'
   match ':controller(/:action(/:id))', :via => :get
-  get '/sign_up', to: 'home#sign_up'
+  post '/sign_up', to: 'home#sign_up'
   resources :medium_comments
 
   resources :media
