b69c23d5b78a7754afedff58e823ca9c68b690e2
RohanSharma1994 <RohanSharma1994@hotmail.com>
2015-04-18 21:04:36 +1000

    Added sign_up AJAX method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,27 @@
 class HomeController < ApplicationController
 	def showHome
 	end
+
 	def sign_up
+		# Retrieve the variables passed in
+		username = params[:username]
+		email = params[:email]
+		password = params[:password]
+		# Need to perform an action and respond
+		# to AJAX request
+		response = ""
+		# Check username exists or email is exists
+		if(User.find_by(email:email)) 
+			response += "emailExists"
+		end
+		if(User.find_by(username:username)) 
+			response += "usernameExists"
+		end
+		# If response is empty, create the user
+		if(response.empty?)
+			User.create(username:username, email:email, password:password)
+		end
+		# Return the response to the AJAX script
+		render :text => response
 	end
 end
