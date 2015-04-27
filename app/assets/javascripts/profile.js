e68a546803755a07e60aa42c19843fcb56896306
RohanSharma1994 <RohanSharma1994@hotmail.com>
2015-04-27 11:52:29 +1000

    Profile updated regex added

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,17 +1,7 @@
 /* A javascript file which handles signing in */
-		document.getElementById('name_outcome').innerHTML = "Invalid username";
-
-function update_profile() {
-	var username = document.getElementById('name').value;
-	var email = document.getElementById('email').value;
-	if(!validate(username, email)) {
-		return false;
-	}
-	return false;
-}
 
 // You should comment on what is allowed in the regex string...
-function validate(username, email) {
+function validate2(username, email) {
 	var regex = /^[A-Za-z0-9]{6,30}$/i;
 	var valid = true;
 	var regex2 = /^[A-Za-z0-9_.-]+@[A-Za-z0-9]+\.[A-Za-z0-9.]*[A-Za-z]$/i;
@@ -25,3 +15,18 @@ function validate(username, email) {
 	}
 	return valid;
 }
+
+function clearErrorMessage() {
+	document.getElementById('name_outcome').innerHTML = "";
+	document.getElementById('email_outcome').innerHTML = "";
+}
+
+
+
+function update_profile() {
+	var username = document.getElementById('name').value;
+	var email = document.getElementById('email').value;
+	var result = validate2(username, email);
+	return result;
+}
+
