97d0c56b28222ee0c5d182615a1b5f8fa66c0f09
Josh Wright <joshuawright11@gmail.com>
2015-05-05 12:46:52 +1000

    Jplayer added to main

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -82,6 +82,25 @@ $('.grind').append(html);
 // Enable these functionalities when page is loaded   
 $(document).ready(function(){
 
+	$("#jquery_jplayer_1").jPlayer({
+		ready: function () {
+			$(this).jPlayer("setMedia", {
+				title: "Bubble",
+				m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+				oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+			});
+		},
+		cssSelectorAncestor: "#jp_container_1",
+		swfPath: "js",
+		supplied: "m4a, oga",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true,
+		remainingDuration: true,
+		toggleDuration: true
+	});
+
     // Generates new songs for the grind
     $('.new-songs').click(function(){
         var interval = null;
