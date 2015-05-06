e69bbad8d328458dc7cb5dd41e958a7abba3e926
RohanSharma1994 <RohanSharma1994@hotmail.com>
2015-05-06 22:10:29 +1000

    Music streams when you click the play button!

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-	console.log("Main js ran");
+/* A function which takes in the track name and file path
+ * of a track and loads it into the JPlayer.
+ */
+function jplayer_load(name, path) {
+	console.log("JPlayer song loaded.");
+	$("#jquery_jplayer_1").jPlayer("destroy");
 	$("#jquery_jplayer_1").jPlayer({
 	    ready: function () {
-	      $(this).jPlayer("setMedia", {
-	        title: "Bubble",
-	        m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
-	        oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-	      });
+	      $(this).jPlayer("setMedia", {title: name,mp3: "assets/media/"+path}).jPlayer("play",0);
 	    },
 	    cssSelectorAncestor: "#jp_container_1",
 	    swfPath: "js",
-	    supplied: "m4a, oga",
+	    supplied: "mp3",
 	    useStateClassSkin: true,
 	    autoBlur: false,
 	    smoothPlayBar: true,
@@ -18,4 +18,6 @@ $(document).ready(function(){
 	    remainingDuration: true,
 	    toggleDuration: true
 	});
-});
+}
+
+
