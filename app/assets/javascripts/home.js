e71dd4f49ed1a41ee8e9271bda16998575db5940
RohanSharma1994 <RohanSharma1994@hotmail.com>
2015-05-17 01:07:19 +1000

    Hide carousel text when not hovering

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -108,7 +108,7 @@ function update_grind(songs) {
 						'<img class="playtrack" src="assets/images/play.png"></a>';
 
 			comment = '<a href="/comments?id='+ track_comment +'"><img class="comment" src="assets/images/comment.ico"></a>';
-			playlist = '<a onclick="parent.add_playlist(\''+track_path+'\')"><img class="add_playlist" src="assets/images/add_playlist.png"></a>';
+			playlist = '<a onclick="parent.add_playlist(\''+track_path+'\')" tabindex="0" role="button" data-toggle="popover" data-trigger="focus" title="Dismissible popover" data-content="And heres some amazing content. Its very engaging. Right?"><img class="add_playlist" src="assets/images/add_playlist.png"></a>';
 
 			html += '<div onclick="" class="element"><img id="track" class="col-md-2 img-thumbnail" style="height:170px;width:170px;" alt="" src="assets/mediaimage/'+ track_img +
 					'"><div class="grid_description">'+ track_info +'</div>'+ playtrack + playlist + comment+'</div>';
