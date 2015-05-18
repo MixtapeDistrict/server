792e5193d5b57d4e76f8354e66b4cc04189fd154
RohanSharma1994 <RohanSharma1994@hotmail.com>
2015-05-18 14:59:38 +1000

    Unit tests for homepage + playlist done

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -9,7 +9,47 @@ class HomeControllerTest
     	puts "Testing Sign up"
     	# Create a dummy user with dummy information
     	user = User.create(username:"hitplayshare",password:"testing",email:"x@hotmail.com")
+    	# Store the id of this user for later tests
+    	@id = user.id
     	# Ensure the user signed up
     	raise "Failed to sign the user up " unless user
+    	puts "Sign up test passed"
     end
+
+    # Tests whether the user can log in
+    def log_in
+    	puts "Testing correct log in"
+    	# Put in the username & password user signed up with
+    	username = "hitplayshare"
+    	password = "testing"
+    	# Ensure the user can log in
+    	user = User.find_by(username:username, password:password)
+    	raise "Failed the log in test" unless user
+    	puts "Log in correct test passed"
+    	puts "Testing incorrect log in"
+    	# Put in the wrong username/password
+    	username = "testing2"
+    	password = "hitplayshare"
+    	user = User.find_by(username:username, password:password)
+    	raise "Incorrect log in test failed" unless not user
+    	puts "Incorrect login test passed"
+    end
+
+    # Clean up
+    def clean_up
+    	puts "Cleaning up..."
+    	User.find_by(id:@id).destroy
+    end
+
+    # A class method which can be called to run this unit test
+    def self.run
+    	# Create a new test object
+    	hct = HomeControllerTest.new
+    	# Run the individual unit tests
+    	puts "<--------------------- Running unit tests for home controller ---------------->"
+    	hct.sign_up
+    	hct.log_in
+    	hct.clean_up
+    	puts "<----------------------ALL UNIT TESTS PASSED-------------------------->"
+	end
 end
--- test/controllers/musics_controller_test.rb
+++ test/controllers/musics_controller_test.rb
@@ -1,49 +1,8 @@
-require 'test_helper'
+# Unit tests for things the music controllerS does:
+# Allows users to upload music,  download music ,play music
+# delete music & edit music.
 
-class MusicsControllerTest < ActionController::TestCase
-  setup do
-    @music = musics(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:musics)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create music" do
-    assert_difference('Music.count') do
-      post :create, music: { genre: @music.genre, image_path: @music.image_path, media_id: @music.media_id, plays: @music.plays }
-    end
-
-    assert_redirected_to music_path(assigns(:music))
-  end
-
-  test "should show music" do
-    get :show, id: @music
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @music
-    assert_response :success
-  end
-
-  test "should update music" do
-    patch :update, id: @music, music: { genre: @music.genre, image_path: @music.image_path, media_id: @music.media_id, plays: @music.plays }
-    assert_redirected_to music_path(assigns(:music))
-  end
-
-  test "should destroy music" do
-    assert_difference('Music.count', -1) do
-      delete :destroy, id: @music
-    end
-
-    assert_redirected_to musics_path
-  end
+# Unit tests for the musics controller
+class MusicsControllerTest 
+  
 end
